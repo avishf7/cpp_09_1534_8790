8104f95d8fdad595c62b39865de19886bae05d4e
avishf7 <62483176+avishf7@users.noreply.github.com>
2020-07-02 22:13:57 +0300

    'בעזרת ה

---
+++ cpp_09_01_1534_8790/SearchTree.h
@@ -0,0 +1,189 @@
+#ifndef __SEARCH_TREE_H_
+#define __SEARCH_TREE_H_
+
+#include "Tree.h"
+#include "funcs.h"
+
+template <typename T, typename Key>
+class SearchTree : public BinaryTree<T, Key> {
+	using typename BinaryTree<T, Key>::GetKey;
+	using typename BinaryTree<T, Key>::Node;
+	using BinaryTree<T, Key>::root;
+	using BinaryTree<T, Key>::_getKey;
+	using BinaryTree<T, Key>::_delete;
+public:
+	SearchTree() {}
+	SearchTree(GetKey getKey) : BinaryTree<T, Key>(getKey) {}
+	SearchTree(const SearchTree&) = delete;
+	SearchTree(SearchTree&&) = delete;
+	SearchTree& operator=(const SearchTree&) = delete;
+	SearchTree& operator=(SearchTree&&) = delete;
+
+	// protocol for search trees
+	void add(T& value) override;
+	bool check(const Key& key) const override { return check(root, key); }
+	T search(const Key& key) const override { return search(root, key); }
+	void remove(const Key&) override;
+	
+	int level(const Key& key) const;
+	Node* ascended(Node*);
+
+private:
+	void add(Node*, T&);
+	bool check(Node* current, const Key& key) const;
+	T search(Node* current, const Key& key) const;
+	int level(Node* current, const Key& key) const;
+	Node* getNode(Node* current, const Key& key) const;
+	Node* getNode(const Key& key) const { return getNode(root, key); }
+	void remove(Node* current, const Key& key);
+};
+
+template <typename T>
+class SearchTreeSimple : public SearchTree<T, T> {
+public:
+	SearchTreeSimple() {}
+	SearchTreeSimple(const SearchTreeSimple&) = delete;
+	SearchTreeSimple(SearchTreeSimple&&) = delete;
+	SearchTreeSimple& operator=(const SearchTreeSimple&) = delete;
+	SearchTreeSimple& operator=(SearchTreeSimple&&) = delete;
+};
+
+template <typename T, typename Key>
+void SearchTree<T, Key>::add(T& value) {
+	// add value to binary search tree
+	if (root == nullptr) {
+		root = new Node(value);
+	}
+	else
+		add(root, value);
+}
+
+template <typename T, typename Key>
+bool SearchTree<T, Key>::check(Node* current, const Key& key) const {
+	// see if argument value occurs in tree
+	if (current == nullptr)
+		return false; // not found
+	if (_getKey(current->value()) == key)
+		return true;
+	if (_getKey(current->value()) < key)
+		return check(current->right(), key);
+	else
+		return check(current->left(), key);
+}
+
+template <typename T, typename Key>
+T SearchTree<T, Key>::search(Node* current, const Key& key) const {
+	// see if argument value occurs in tree
+	if (current == nullptr)
+		throw "not found:"; // not found
+	if (_getKey(current->value()) == key)
+		return current->value();
+	if (_getKey(current->value()) < key)
+		return search(current->right(), key);
+	else
+		return search(current->left(), key);
+}
+
+template<typename T, typename Key>
+int SearchTree<T, Key>::level(Node* current, const Key& key) const
+{
+	if (current == nullptr)
+		return -1; // not found
+	if (_getKey(current->value()) == key)
+		return 0;
+	if (_getKey(current->value()) < key)
+		return 1 + level(current->right(), key);
+	else
+		return 1 + level(current->left(), key);
+}
+
+template <typename T, typename Key>
+typename SearchTree<T, Key>::Node* SearchTree<T, Key>::getNode(Node* current, const Key& key) const {
+	// see if argument value occurs in tree
+	if (current == nullptr)
+		throw "not found:"; // not found
+	if (_getKey(current->value()) == key)
+		return current;
+	if (_getKey(current->value()) < key)
+		return getNode(current->right(), key);
+	else
+		return getNode(current->left(), key);
+}
+
+template <typename T, typename Key>
+void SearchTree<T, Key>::add(Node* current, T& value) {
+	if (_getKey(current->value()) < _getKey(value))
+		// add to right subtree
+		if (current->right() == nullptr)
+			current->right(new Node(value, current));
+		else
+			add(current->right(), value);
+	else
+		// add to left subtree
+		if (current->left() == nullptr)
+			current->left(new Node(value, current));
+		else
+			add(current->left(), value);
+}
+
+template <typename T, typename Key>
+void SearchTree<T, Key>::remove(Node* current, const Key& key) {
+
+	if (current->right())
+	{
+		Node* ascNode = ascended(current);
+		mySwap(current->value(), ascNode->value());
+		remove(ascNode,_getKey(ascNode->value()));
+	}
+	else
+	{
+		if (current->left())
+		{
+			current->right()->parent(current->parent());
+
+			if (current->parent()->right() == current)
+				current->parent()->right(current->left());
+			else
+				current->parent()->left(current->left());
+		}
+		else
+		{
+			if (current->parent()->right() == current)
+				current->parent()->right(nullptr);
+			else
+				current->parent()->left(nullptr);
+		}
+
+		_delete(current->value());
+		delete current;
+	}
+}
+
+template<typename T, typename Key>
+void SearchTree<T, Key>::remove(const Key& key)
+{
+	remove(root, key);
+}
+
+template <typename T, typename Key>
+int SearchTree<T, Key>::level(const Key& key) const {
+
+	return level(root, key);
+}
+
+template<typename T, typename Key>
+typename SearchTree<T, Key>::Node* SearchTree<T, Key>::ascended(SearchTree<T, Key>::Node* node)
+{
+	if (!node || !node->right())
+		return nullptr;
+
+	if (!node->right()->left())
+		return node->right();
+
+	auto i = node->right();
+	while (i->left()) { i = i->left(); };
+
+	return i;
+}
+
+#endif // __SEARCH_TREE_H_
---
+++ cpp_09_01_1534_8790/Tree.h
@@ -0,0 +1,238 @@
+#pragma once
+#include <iostream>
+#include "funcs.h"
+
+enum Mode { PREORDER, INORDER, POSTORDER };
+
+//-----------------------------------
+// class BinaryTree (Binary Trees)
+// process nodes in Pre/In/Post order
+//-----------------------------------
+template <typename T, typename Key>
+class BinaryTree {
+	Mode _mode;
+
+protected:
+	//--------------------------------------------------------
+	// inner class Node
+	// a single Node from a binary tree
+	//--------------------------------------------------------
+	class Node {
+	protected:
+		Node* _parent;
+		Node* _left;
+		Node* _right;
+		T _value;
+	public:
+		Node(T& value, Node* const parent = nullptr, Node* const left = nullptr, Node* const right = nullptr)
+			: _value(value), _parent(parent), _left(left), _right(right) {}
+		T& value() { return this->_value; }
+		void value(const T& value) { _value = value; }
+		Node* parent() const { return _parent; }
+		void parent(Node* node) { _parent = node; }
+		Node* left() const { return _left; }
+		void left(Node* node) { _left = node; }
+		Node* right() const { return _right; }
+		void right(Node* node) { _right = node; }
+
+	}; //end of Node class
+
+
+	using GetKey = Key(*)(const T&);
+	using ValueProc = void(*)(T&);
+
+	GetKey _getKey;
+	ValueProc _delete;
+
+	Node* root;
+	virtual void process(Node& node, ValueProc process) const { process(node.value()); }
+
+public:
+
+	BinaryTree(GetKey getKey, ValueProc del)
+		: root(nullptr), _mode(INORDER), _getKey(getKey), _delete(del) {}
+	BinaryTree() : BinaryTree([](const T& value) { return static_cast<Key>(value); },
+		[](T& value) {}) {}
+
+	virtual ~BinaryTree() { if (root != nullptr) clear(root); }
+
+	BinaryTree(const BinaryTree&) = delete;
+	BinaryTree(BinaryTree&&) = delete;
+	BinaryTree& operator=(const BinaryTree&) = delete;
+	BinaryTree& operator=(BinaryTree&&) = delete;
+
+	void mode(Mode mode) { _mode = mode; }
+	Mode mode() const { return _mode; }
+
+	bool isEmpty() const { return root == nullptr; }
+	void clear() { clear(root); root = nullptr; }
+	void process(ValueProc) const;
+	void process() const { process([](T& value) { std::cout << value << " "; }); };
+
+	virtual void add(T&) = 0;
+	virtual bool check(const Key&) const = 0;
+	virtual T search(const Key&) const = 0;
+	virtual void remove(const Key&) = 0;
+
+private:
+	void preOrder(ValueProc process) const { preOrder(root, process); }
+	void inOrder(ValueProc process) const { inOrder(root, process); }
+	void postOrder(ValueProc process) const { postOrder(root, process); }
+	void clear(Node*);
+	int leaves(Node*) const;
+	int height(Node*) const;
+	void swapSons(Node*);
+	void reflect(Node*);
+	int onlyLeftSons(Node*) const;
+	void preOrder(Node*, ValueProc) const;
+	void inOrder(Node*, ValueProc) const;
+	void postOrder(Node*, ValueProc) const;
+
+public:
+
+	int leaves() const;
+	int height() const;
+	int onlyLeftSons() const;
+	void reflect();
+
+
+
+};
+
+//----------------------------------------------------------
+// class BinaryTree implementation
+//----------------------------------------------------------
+template <typename T, typename Key>
+void BinaryTree<T, Key>::clear(Node* current) {
+	if (current != nullptr) { // Release memory associated with children
+		clear(current->left());
+		clear(current->right());
+		_delete(current->value());
+		delete current;
+	}
+}
+
+template<typename T, typename Key>
+int BinaryTree<T, Key>::leaves(Node* node) const
+{
+	if (!node)
+		return 0;
+
+	if (!node->right() && !node->left())
+		return 1;
+
+	return leaves(node->right()) + leaves(node->left());
+}
+
+template<typename T, typename Key>
+int BinaryTree<T, Key>::height(Node* node) const
+{
+	if (!node)
+		return -1;
+
+	return max<int>(1 + height(node->left()), 1 + height(node->right()));
+}
+
+template<typename T, typename Key>
+void BinaryTree<T, Key>::swapSons(Node* node)
+{
+	Node& temp = *(node->left());
+	node->left(node->right());
+	node->right(&temp);
+}
+
+template<typename T, typename Key>
+void BinaryTree<T, Key>::reflect(Node* node)
+{
+	if (!node)
+		return;
+
+	swapSons(node);
+	reflect(node->left());
+	reflect(node->right());
+}
+
+template<typename T, typename Key>
+int BinaryTree<T, Key>::onlyLeftSons(Node* node) const
+{
+	if (!node || (!node->right() && !node->left()))
+		return 0;
+
+	if (node->left() && !node->right())
+		return (1 + onlyLeftSons(node->left()));
+
+	return onlyLeftSons(node->left()) + onlyLeftSons(node->right());
+}
+
+// preOrder processing of tree rooted at current
+template <typename T, typename Key>
+void BinaryTree<T, Key>::preOrder(Node* current, ValueProc proc) const {
+	// visit Node, left child, right child
+	if (current != nullptr) { // process current Node
+		process(*current, proc);
+		// then visit children
+		preOrder(current->left(), proc);
+		preOrder(current->right(), proc);
+	}
+}
+// inOrder processing of tree rooted at current
+template <typename T, typename Key>
+void BinaryTree<T, Key>::inOrder(Node* current, ValueProc proc) const {
+	// visit left child, Node, right child
+	if (current != nullptr) {
+		inOrder(current->left(), proc);
+		process(*current, proc);
+		inOrder(current->right(), proc);
+	}
+}
+// postOrder processing of tree rooted at current
+template <typename T, typename Key>
+void BinaryTree<T, Key>::postOrder(Node* current, ValueProc proc) const {
+	// visit left child, right child, node
+	if (current != nullptr) {
+		postOrder(current->left(), proc);
+		postOrder(current->right(), proc);
+		process(*current, proc);
+	}
+}
+
+template <typename T, typename Key>
+void BinaryTree<T, Key>::process(ValueProc process) const {
+	switch (_mode) {
+	case PREORDER:
+		preOrder(process);
+		break;
+	case INORDER:
+		inOrder(process);
+		break;
+	case POSTORDER:
+		postOrder(process);
+		break;
+	}
+}
+
+template <typename T, typename Key>
+int BinaryTree<T, Key>::leaves()  const {
+	return leaves(root);
+}
+
+template <typename T, typename Key>
+int BinaryTree<T, Key>::height() const {
+	return height(root);
+}
+
+template <typename T, typename Key>
+int BinaryTree<T, Key>::onlyLeftSons()  const {
+	return onlyLeftSons(root);
+}
+
+template <typename T, typename Key>
+void BinaryTree<T, Key>::reflect() {
+	reflect(root);
+}
+
+
+
+
+
+
